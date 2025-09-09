d4c28c636a44d371bdbbf18b4c4a0c0580abbc26
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2025-09-10 02:14:53 +0700

    +powershell

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,14 +11,22 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 
+# PowerShell
+mkdir -p "/var/opt" && ln -s "/var/opt" "/opt"
+mkdir -p "/var/usrlocal" && ln -s "/var/usrlocal" "/usr/local"
+
+dnf5 config-manager addrepo --from-repofile=https://packages.microsoft.com/config/rhel/9/prod.repo --save-filename=microsoft-prod.repo
+dnf5 install -y powershell
+
 # VS Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf5 install -y code
 
 # Other softwares
 echo defaultyes=True | tee -a /etc/dnf/dnf.conf
 dnf5 config-manager setopt terra.enabled=1
-dnf5 install -y code gparted gsmartcontrol cascadia-fonts-all coolercontrol android-tools java-21-openjdk usbview
+dnf5 install -y gparted gsmartcontrol cascadia-fonts-all coolercontrol android-tools java-21-openjdk usbview
 
 # Use a COPR Example:
 #
