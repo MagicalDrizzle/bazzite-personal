d1d427fd5f36147454104183eabfc3f2e21168e7
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2025-10-30 01:08:28 +0700

    get pwsh if PMC is down

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,9 +15,15 @@ set -ouex pipefail
 mkdir -p "/var/opt" && ln -s "/var/opt" "/opt"
 mkdir -p "/var/usrlocal" && ln -s "/var/usrlocal" "/usr/local"
 
-dnf5 config-manager addrepo --from-repofile=https://packages.microsoft.com/config/rhel/9/prod.repo --save-filename=microsoft-prod.repo
-dnf5 install -y powershell
-sed -zi 's@enabled=1@enabled=0@' /etc/yum.repos.d/microsoft-prod.repo
+timeout 5s ping -qc 3 packages.microsoft.com
+if [ $? -eq 0 ]; then
+    dnf5 config-manager addrepo --from-repofile=https://packages.microsoft.com/config/rhel/9/prod.repo --save-filename=microsoft-prod.repo
+    dnf5 install -y powershell
+    sed -zi 's@enabled=1@enabled=0@' /etc/yum.repos.d/microsoft-prod.repo
+else
+    pwver=$(curl -s https://api.github.com/repos/PowerShell/PowerShell/tags | grep -Pom 1 '[0-9]+\.[0-9]+\.[0-9](?=")')
+    dnf5 install -y https://github.com/PowerShell/PowerShell/releases/download/v$pwver/powershell-$pwver-1.rh.x86_64.rpm
+fi
 
 # VS Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
